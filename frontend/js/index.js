233347a31289893c843b22a808a4ea52f051d691
mccleanjosef <josefmcclean@gmail.com>
2022-04-06 23:26:48 +1200

    responsive masonry layout with left to right order and animated on window resize - using Magic Grid library

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -3,21 +3,35 @@ console.log(sessionStorage);
 
 $(document).ready(function() {
 
+  // ============ Masonry Layout - Magic Grid Starts ============
+  let magicGrid = new MagicGrid({
+    container: '.container',
+    animate: true,
+    gutter: 30,
+    static: true,
+    useMin: true
+  });
 
-    let url; //declare url as a variable in es6
-    $.ajax({
-      url: 'config.json',
-      type: 'GET',
-      dataType: 'json',
-      success: function(configData) {
-        console.log(configData.SERVER_URL, configData.SERVER_PORT);
-        url = `${configData.SERVER_URL}:${configData.SERVER_PORT}`;
-        console.log(url);
-      },
-      error: function(error) {
-        console.log(error);
-      }
-    })
+  magicGrid.listen();
+  // ============ Masonry Layout - Magic Grid Ends ============
+
+
+  let url; //declare url as a variable in es6
+  $.ajax({
+    url: 'config.json',
+    type: 'GET',
+    dataType: 'json',
+    success: function(configData) {
+      console.log(configData.SERVER_URL, configData.SERVER_PORT);
+      url = `${configData.SERVER_URL}:${configData.SERVER_PORT}`;
+      console.log(url);
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  })
+
+  
 
 
 }); //document.ready
