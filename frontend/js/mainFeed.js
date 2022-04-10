0ab07d48ea673a0cb878c26367904dbedfef8e45
mccleanjosef <josefmcclean@gmail.com>
2022-04-11 01:31:15 +1200

    Masonry working with appended posts and Mongo Data JM

--- frontend/js/mainFeed.js
+++ frontend/js/mainFeed.js
@@ -3,13 +3,15 @@ $(document).ready(function() {
 
     // ============ Masonry Layout - Masonry init Starts ============
     // init Masonry
-    $('.grid').masonry({
-        // options
-        itemSelector: ".grid-item",
-        columnWidth: ".grid-item",
-        gutter: 10,
-        horizontalOrder: true,
-        fitWidth: true
+    $( function() {
+        $('.grid').masonry({
+            // options
+            itemSelector: ".post",
+            columnWidth: 341,
+            gutter: 10,
+            horizontalOrder: true,
+            fitWidth: true
+        });
     });
 
     // layout Masonry after each image loads
@@ -31,54 +33,7 @@ $(document).ready(function() {
         console.log(configData.SERVER_URL, configData.SERVER_PORT);
         url = `${configData.SERVER_URL}:${configData.SERVER_PORT}`;
         console.log(url);
-        viewProjects(); //called here to use url in allProjectsFromDB on page load
-
-        let testId = [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113];
-
-        // Generate all posts when document is ready
-        let i;
-        document.getElementById('postContainer').innerHTML = '';
-
-        for(i=0; i<testId.length; i++){
-        document.getElementById('postContainer').innerHTML +=
-        // Adding post elements here
-        `
-        <div class="post" id="${testId[i]}">
-            <div class="post__top">
-                <div class="post__author-img-wrap">
-                    <img class="post__author-image" src="https://images.unsplash.com/photo-1482046187924-50f27dc64333?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Author profile image">
-                </div>
-                <div class="post__btn-ctn">
-                    <button type="button" class="btn dropdown-toggle post__post-dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img class="post__dropdown-icon" src="./img/post-dropdown-icon.svg" alt="post dropdown icon">
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-end post__dropdown-menu">
-                        <li><button class="dropdown-item post__dropdown-item" type="button" data-bs-toggle="modal" data-bs-target=".main-feed-update-post-modal">Update</button></li>
-                        <li><button class="dropdown-item post__dropdown-item" type="button" data-bs-toggle="modal" data-bs-target=".main-feed-delete-post-modal">Delete</button></li>
-                    </ul>
-                </div>
-                <img class="post__image" src="./img/tui.jpg" alt="User posted picture">
-            </div>
-            <div class="post__bottom">
-                <div class="post__bottom-upper">
-                    <p class="post__text">Kahurangi National Park</p>
-                    <p class="post__text">Kea</p>
-                </div>
-
-                <div class="post__bottom-lower">
-                    <p class="post__caption">Check out this curious fella!</p>
-                </div>
-
-                <div class="post__comments">
-                    <p class="post__comments-details">view details</p>
-                    <img class="post__speech" src="./img/comments-icon.svg" alt="comments icon">
-                </div>
-            </div>
-        </div>
-        `;
-        } //end of for loop
-
-        
+        allPosts(url);
     },
     error: function(error) {
         console.log(error);
@@ -105,52 +60,75 @@ $(document).ready(function() {
 
 
 
-    //============ Posts Starts ============
-  
-    // View all cards when document is ready
-    function viewProjects(){
+    //============ All Posts Starts ============
+    // Getting all posts once main-feed is ready
+    function allPosts(url){
         $.ajax({
-            url: `http://${url}/allProjectsFromDB`,
+            url: `http://${url}/allPostsFromDB`,
             type: 'GET',
-            datatype: 'json',
-            success: function(projectsFromMongo){
-                // console.log(projectsFromMongo);
-                
-                let i;
-                document.getElementById('result').innerHTML = '';
-                for(i=0; i<projectsFromMongo.length; i++){
-                    document.getElementById('result').innerHTML +=
-                    // Adding project-card elements here
+            dataType: 'json',
+            success:function(postsFromMongo){
+    
+                document.getElementById('postContainer').innerHTML = '';
+                for(i = 0; i < postsFromMongo.length; i++){
+                    
+                    document.getElementById('postContainer').innerHTML +=
                     `
-                    <div id="${projectsFromMongo[i]._id}" class="projects__card">
-                        <button class="projects__project-options"></button>
-                        <div class="projects__dropdown-content">
-                            <a class="updateBtn">Update</a>
-                            <a class="deleteBtn">Delete</a>
-                        </div>
-                        <a class="projects__portfolio-link" href="${projectsFromMongo[i].project_url}"  target="blank">
-                            <div class="projects__image-wrap">
-                                <img class="projects__img" src="${projectsFromMongo[i].image_url}" alt="project image">
+                    <div class="post">
+                        <div class="post__top">
+                            <div class="post__author-img-wrap">
+                                <img class="post__author-image" src="https://images.unsplash.com/photo-1482046187924-50f27dc64333?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Author profile image">
                             </div>
-                        </a>
-                        <h1 class="projects__heading hide">${projectsFromMongo[i].name}</h1>
-                        <a class="projects__small-screen-link hide" href="${projectsFromMongo[i].project_url}"  target="blank">Go to project<img class="projects__link-icon" src="../frontend/assets/click-screen-link-icon.svg" alt="project link icon"></a>
-                        <h3 class="projects__author hide">${projectsFromMongo[i].username}</h3>
-                        <div class="projects__description-wrap hide">
-                            <p class="projects__description" id="123${projectsFromMongo[i]._id}">${projectsFromMongo[i].description}</p>
+                            <div class="post__btn-ctn">
+                                <button type="button" class="btn dropdown-toggle post__post-dropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img class="post__dropdown-icon" src="./img/post-dropdown-icon.svg" alt="post dropdown icon">
+                                </button>
+                                <ul class="dropdown-menu dropdown-menu-end post__dropdown-menu">
+                                    <li><button class="dropdown-item post__dropdown-item" type="button" data-bs-toggle="modal" data-bs-target=".main-feed-update-post-modal">Update</button></li>
+                                    <li><button class="dropdown-item post__dropdown-item" type="button" data-bs-toggle="modal" data-bs-target=".main-feed-delete-post-modal">Delete</button></li>
+                                </ul>
+                            </div>
+                            <img class="post__image" src="${postsFromMongo[i].image_url}" alt="User posted picture">
+                        </div>
+                        <div class="post__bottom">
+                            <div class="post__bottom-upper">
+                                <p class="post__text">${postsFromMongo[i].location}</p>
+                                <p class="post__text">${postsFromMongo[i].name}</p>
+                            </div>
+                            <div class="post__bottom-lower">
+                                <p class="post__caption">${postsFromMongo[i].description}</p>
+                            </div>
+                            <div class="post__comments">
+                                <p class="post__comments-details">view details</p>
+                                <img class="post__speech" src="./img/comments-icon.svg" alt="comments icon">
+                            </div>
                         </div>
                     </div>
-                    `;
-                } //end of for loop
+                    `
+                }
 
-                // following runs in success function so elements can be selected afterwards
+                $('.grid').masonry('destroy'); 
+                $( function() {
+                    $('.grid').masonry({
+                        // options
+                        itemSelector: ".post",
+                        columnWidth: 341,
+                        gutter: 10,
+                        horizontalOrder: true,
+                        fitWidth: true
+                    });
+                });
 
-            },
+                // layout Masonry after each image loads
+                $('.grid').imagesLoaded().progress(function() {
+                    $('.grid').masonry("layout");
+                });
+            },//success
             error:function(){
-              alert('unable to get projects');
-            }
-        }) //ajax
-    }
-    //------- project cards ends ---------
+                alert('Unable to get posts');
+            }//error
+        })//ajax
+    }//view
+    //------- All Posts Ends ---------
 
 }); //document.ready
