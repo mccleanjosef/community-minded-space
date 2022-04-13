a618f02980b55aa468f33779a8353e5908fc955e
mccleanjosef <josefmcclean@gmail.com>
2022-04-13 17:50:28 +1200

    merge in error messages and beaufied js and html files JM

--- frontend/js/full-screen.js
+++ frontend/js/full-screen.js
@@ -3,47 +3,50 @@ $(document).ready(() => {
     // ============ API Starts ============
     let url; //declare url as a variable in es6
     $.ajax({
-    url: 'config.json',
-    type: 'GET',
-    dataType: 'json',
-    success: function(configData) {
-      console.log(configData.SERVER_URL, configData.SERVER_PORT);
-      url = `${configData.SERVER_URL}:${configData.SERVER_PORT}`;
-      // console.log(url);
-      allPostFromDB(url);
-    },
-    error: function(error) {
-      console.log(error);
-    }
+        url: 'config.json',
+        type: 'GET',
+        dataType: 'json',
+        success: function(configData) {
+            console.log(configData.SERVER_URL, configData.SERVER_PORT);
+            url = `${configData.SERVER_URL}:${configData.SERVER_PORT}`;
+            // console.log(url);
+            allPostFromDB(url);
+        },
+        error: function(error) {
+            console.log(error);
+        }
     })
     // ============ API Ends ============
 
+
     // ============ Fullscreen append starts ============
     const allPostFromDB = () => {
-    $.ajax({
-        url: `http://${url}/allPostsFromDB`,
-        type: 'GET',
-        dataType: 'json',
-        success:function(postsFromMongo){
-            let i;
-            for(i=0; i < postsFromMongo.length; i++){
-            $('#fullscreenInner').append(
+        $.ajax({
+            url: `http://${url}/allPostsFromDB`,
+            type: 'GET',
+            dataType: 'json',
+            success: function(postsFromMongo) {
+                let i;
+                for (i = 0; i < postsFromMongo.length; i++) {
+                    $('#fullscreenInner').append(
 
-            `
+                        `
             <div class="carousel-item  ${i === 0 ? 'active' : '' }" data-bs-interval="20000">
               <div class="fullscreen__mobile-header">
                 <div class="fullscreen__profile-image-wrap">
-                  <img class="fullscreen__user-image" src="${postsFromMongo[i].profile_img}" alt="Author profile image">
+                  <img class="fullscreen__profile-image" src="${postsFromMongo[i].profile_img}" alt="Author profile image">
                 </div>
                 <p class="fullscreen__username">${postsFromMongo[i].username}</p>
               </div>
-              <div class="fullscreen__image-wrap">
-                <img class="fullscreen__user-image" src="${postsFromMongo[i].image_url}" alt="Author profile image">
+              <div class="fullscreen__image-ctn">
+                <div class="fullscreen__image-wrap">
+                  <img class="fullscreen__user-image" src="${postsFromMongo[i].image_url}" alt="Author profile image">
+                </div>
               </div>
               <div class="fullscreen__post-details">
                 <div class="fullscreen__desktop-header">
                   <div class="fullscreen__profile-image-wrap">
-                    <img class="fullscreen__user-image" src="${postsFromMongo[i].profile_img}" alt="Author profile image">
+                    <img class="fullscreen__profile-image" src="${postsFromMongo[i].profile_img}" alt="Author profile image">
                   </div>
                   <p class="fullscreen__username">${postsFromMongo[i].username}</p>
                 </div>
@@ -55,32 +58,14 @@ $(document).ready(() => {
               </div>
             </div>
             `
-            );
-          }
+                    );
+                }
 
-          // <div class="fullscreen__user-container">
-          //   <img class="fullscreen__user-image" src="${postsFromMongo[i].profile_img}" alt="Author profile image">
-          //   <p class="fullscreen__username">${postsFromMongo[i].username}</p>
-          // </div>
-
-          // <div class="fullscreen__img-container">
-          //   <img src="${postsFromMongo[i].image_url}" class="fullscreen__image" alt="...">
-          // </div>
-          
-              
-          // <div class="fullscreen__body">
-          //   <p class="fullscreen__text">${postsFromMongo[i].location}</p>
-          //   <p class="fullscreen__text">${postsFromMongo[i].name}</p>
-          // </div>
-
-          // <div class="fullscreen__description-container">
-          //   <p class="fullscreen__description">${postsFromMongo[i].description}</p>
-          // </div>
-        },
-        error:function(){
-            alert('Unable to view fullscreen');
-        }//error
-    })
-}
-// ============ Fullscreen append ends ============
+            },
+            error: function() {
+                alert('Unable to view fullscreen');
+            } //error
+        })
+    }
+    // ============ Fullscreen append ends ============
 })
